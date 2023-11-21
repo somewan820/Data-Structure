38d22d568e05ef122b05a999607182f74858dc6c
somewan <144975606+somewan820@users.noreply.github.com>
2023-11-21 20:29:15 +0800

    Delete Link list/build directory

--- Link list/build/CMakeFiles/proj_first.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for proj_first.
-# This may be replaced when dependencies are built.
--- Link list/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/A/Desktop/code_repos/proj_first
-# Build directory: C:/Users/A/Desktop/code_repos/proj_first/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
