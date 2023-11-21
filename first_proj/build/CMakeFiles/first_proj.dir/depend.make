cefaa2b8945ec7703c5816f50f4edd151ebdca72
some <2176978853@qq.com>
2023-11-21 16:40:15 +0800

    first_proj/main

---
+++ first_proj/build/CMakeFiles/first_proj.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for first_proj.
+# This may be replaced when dependencies are built.
---
+++ first_proj/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/code_repos/first_proj
+# Build directory: D:/code_repos/first_proj/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
