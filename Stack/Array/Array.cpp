3cbddf024a96f569b7390ea149dbe165b2c4c8ba
some <2176978853@qq.com>
2023-12-05 20:45:03 +0800

    Array

--- Stack/Array/Array.cpp
+++ Stack/Array/Array.cpp
@@ -4,20 +4,18 @@
 int top = -1;
 int a[10];
 
-void Push(int x){
+void Push(int x) {
     top = top + 1;
     a[top] = x;
 }
 
-void Pop(){
-    top = top - 1;
-}
+void Pop() { top = top - 1; }
 
-int Top(){
-    std::cout << a[top];
-}
+int Top() { std::cout << a[top]; }
 
-bool Isempty(){
-    if(top>=0) return true;
-    else return false;
+bool Isempty() {
+    if (top >= 0)
+        return true;
+    else
+        return false;
 }
--- Stack/Link list/LInk List.cpp
+++ Stack/Link list/LInk List.cpp
@@ -1,49 +1,41 @@
 //使用链表实现一个栈
 #include <iostream>
 //定义链表节点的结构体
-struct Node
-{
+struct Node {
     int data;
-    Node* next;
+    Node *next;
 };
 //定义头节点
-Node* head = NULL;
+Node *head = NULL;
 //在栈顶插入数据
-void Push(int x)
-{
-    Node* temp = new Node;
+void Push(int x) {
+    Node *temp = new Node;
     temp->data = x;
     temp->next = head;
     head = temp;
 }
 //删除栈顶数据
-void Pop()
-{
-    Node* temp = head;
+void Pop() {
+    Node *temp = head;
     head = temp->next;
     delete temp;
 }
 //检验是否为空栈
-//using_type -> (std::cout << (Isempty()?"Yes":"No") << std::endl;)
-bool Isempty()
-{
-    return head == NULL;
-}
+// using_type -> (std::cout << (Isempty()?"Yes":"No") << std::endl;)
+bool Isempty() { return head == NULL; }
 //获取栈顶元素
-int Top()
-{
-    if (head == NULL){
+int Top() {
+    if (head == NULL) {
         std::cout << "Stack is empty." << std::endl;
         return -1;
     }
     return head->data;
 }
 //打印链表
-void Print(Node* head)
-{
-    Node* temp = head;
+void Print(Node *head) {
+    Node *temp = head;
     std::cout << "List is:";
-    while(temp!=NULL){
+    while (temp != NULL) {
         std::cout << temp->data << " ";
         temp = temp->next;
     }
